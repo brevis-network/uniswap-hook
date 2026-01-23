5f514997d56cec4ee521d0093d13e20272e4106a
Junda Liu <517443+i9@users.noreply.github.com>
2026-01-23 13:08:20 -0800

    update runforge

--- runforge.sh
+++ runforge.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
 echo "reqid:" $REQUEST_ID
-forge script script/deployHook.sol --rpc-url "$1" --keystore $2 --password-file "$2.pwd" --broadcast
+/home/ubuntu/.cargo/bin/forge script script/deployHook.sol --rpc-url "$1" --keystore $2 --password-file "$2.pwd" --broadcast \
+    --verify --verifier oklink --verifier-url https://www.oklink.com/api/v5/explorer/contract/verify-source-code-plugin/xlayer --api-key 5dd22fb5-ab0c-4abd-b0a3-bd4e6d54a5b3 \
+    --delay 10 --retries 10
